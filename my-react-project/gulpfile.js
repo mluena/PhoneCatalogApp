526a53fa03be6ccb2a74e125fc686367a57e70b9
María Luena Rodríguez <maluenarod@gmail.com>
2018-09-18 15:55:07 +0200

    with docker

--- my-react-project/gulpfile.js
+++ my-react-project/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('default', function () {
-  gulp.src('scss/main.scss') // Leo el archivo scss
+  gulp.src('./scss/main.scss') // Leo el archivo scss
     .pipe(sass()) // Convierto el contenido del archivo index.scss a CSS
     .pipe(gulp.dest('src/css')); // El CSS generado lo guardamos en la carpeta css
 });
