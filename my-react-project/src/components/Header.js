9c241760fba42de3a64543e5085566b637549d8b
María Luena Rodríguez <maluenarod@gmail.com>
2018-09-18 11:47:08 +0200

    link to masmovil

--- my-react-project/src/components/Header.js
+++ my-react-project/src/components/Header.js
@@ -6,7 +6,9 @@ class Header extends React.Component {
 	render() {
 		return(
             <div className="app-header">
-                <img className="app-logo" alt="logo-másmovil" src={logo} />
+                <a href="https://www.masmovil.es/">
+                    <img className="app-logo" alt="logo-másmovil" src={logo} />
+                </a>
                 <h1 className="app-title">MÓVILES</h1>
                 <h2 className="app-title">Los mejores móviles al precio más bajo.</h2>
             </div>
